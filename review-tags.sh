3e4e1517a3f440410be9471e273d8e7e94d9d4d7
Nic Manoogian <nic.manoogian@doppler.com>
2022-12-01 12:51:20 -0500

    Add review-tags initial impl

---
+++ review-tags.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -eo pipefail
+
+if [[ $# -lt 2 ]]; then
+	echo "usage: review-tags.sh <branch_name> <create|list>"
+	exit
+fi
+
+branch_name="$1"
+if ! git rev-parse "origin/$branch_name" > /dev/null 2>&1; then
+	echo "origin/$branch_name does not exist"
+	exit
+fi
+
+command="$2"
+case $command in
+	c|create)
+		last_tag=$(git tag -l | grep "review/$branch_name" | tail -1)
+
+		if [ -z "$last_tag" ]; then
+			new_num="1"
+		else
+			if [ "$(git rev-parse "$last_tag")" = "$(git rev-parse "origin/$branch_name")" ]; then
+				echo "$last_tag is same as origin"
+				exit
+			fi
+			last_num="${last_tag##*/}"
+			new_num=$((last_num + 1))
+		fi
+
+		new_branch="review/$branch_name/$new_num"
+		git tag "$new_branch" "origin/$branch_name"
+		echo "+ $new_branch"
+		;;
+
+	l|list)
+		git tag -l | grep "review/$branch_name"
+		;;
+
+	d|diff)
+		last_tag=$(git tag -l | grep "review/$branch_name" | tail -1)
+		git range-diff origin/develop "review/$branch_name/$3" "review/$branch_name/$4"
+		;;
+
+	*)
+		echo "Unknown command $command"
+		exit 1
+		;;
+esac
