93c4a8c129a8b655c30d76d536aa16764d95ccaa
Nic Manoogian <nic.manoogian@doppler.com>
2022-12-02 12:32:24 -0500

    Adjust commands to use branch file

--- review-tags.sh
+++ review-tags.sh
@@ -2,52 +2,73 @@
 
 set -eo pipefail
 
-if [[ $# -lt 2 ]]; then
-	echo "usage: review-tags.sh <branch_name> <create|list>"
-	exit
+if [[ $# -lt 1 ]]; then
+  echo "usage: review-tags.sh <command>"
+  exit
 fi
 
-branch_name="$1"
-if ! git rev-parse "origin/$branch_name" > /dev/null 2>&1; then
-	echo "origin/$branch_name does not exist"
-	exit
-fi
+BRANCH_FILE="$HOME/.rt"
+branch_name=''
 
-command="$2"
+fetch_branch_name() {
+  branch_name="$(cat "$BRANCH_FILE")"
+  if ! git rev-parse "origin/$branch_name" > /dev/null 2>&1; then
+    echo "origin/$branch_name does not exist"
+    exit
+  fi
+}
+
+command="$1"
 case $command in
-	c|create)
-		last_tag=$(git tag -l | grep "review/$branch_name" | tail -1)
+  s|stat|status)
+    cat "$BRANCH_FILE"
+    ;;
 
-		if [ -z "$last_tag" ]; then
-			new_num="1"
-		else
-			if [ "$(git rev-parse "$last_tag")" = "$(git rev-parse "origin/$branch_name")" ]; then
-				echo "$last_tag is same as origin"
-				exit
-			fi
-			last_num="${last_tag##*/}"
-			new_num=$((last_num + 1))
-		fi
+  sb|set-branch)
+    if [ -z "$2" ]; then
+      git branch -r | sed 's!origin/!!' | tr -d ' ' | fzf > "$BRANCH_FILE"
+    else
+      echo "$2" > "$BRANCH_FILE"
+    fi
+    ;;
 
-		new_branch="review/$branch_name/$new_num"
-		git tag "$new_branch" "origin/$branch_name"
-		echo "+ $new_branch"
-		;;
+  c|create)
+    fetch_branch_name
+    last_tag=$(git tag -l | grep "review/$branch_name" | tail -1)
 
-	l|list)
-		git tag -l | grep "review/$branch_name"
-		;;
+    if [ -z "$last_tag" ]; then
+      new_num="1"
+    else
+      if [ "$(git rev-parse "$last_tag")" = "$(git rev-parse "origin/$branch_name")" ]; then
+        echo "$last_tag is same as origin"
+        exit
+      fi
+      last_num="${last_tag##*/}"
+      new_num=$((last_num + 1))
+    fi
 
-	d|diff)
-		git range-diff origin/develop "review/$branch_name/$3" "review/$branch_name/$4"
-		;;
+    new_branch="review/$branch_name/$new_num"
+    git tag "$new_branch" "origin/$branch_name"
+    echo "+ $new_branch"
+    ;;
 
-	co|checkout)
-		git checkout "review/$branch_name/$3"
-		;;
+  l|list)
+    fetch_branch_name
+    git tag -l | grep "review/$branch_name"
+    ;;
 
-	*)
-		echo "Unknown command $command"
-		exit 1
-		;;
+  d|diff)
+    fetch_branch_name
+    git range-diff origin/develop "review/$branch_name/$2" "review/$branch_name/$3"
+    ;;
+
+  co|checkout)
+    fetch_branch_name
+    git checkout "review/$branch_name/$2"
+    ;;
+
+  *)
+    echo "Unknown command $command"
+    exit 1
+    ;;
 esac
