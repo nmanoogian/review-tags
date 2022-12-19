b1da9a36fbee5af30512b586635f75c27fc2fe6a
Franklin Davenport <franklin@dvn.us>
2022-12-19 12:31:50 -0600

    Use tracking branch instead of .rt file

--- review-tags.sh
+++ review-tags.sh
@@ -7,14 +7,16 @@ if [[ $# -lt 1 ]]; then
   exit 1
 fi
 
-BRANCH_FILE="$HOME/.rt"
-touch "$BRANCH_FILE"
 branch_name=''
 
+read_tracking_branch() {
+  git for-each-ref --format='%(upstream:short)' "$(git symbolic-ref -q HEAD)"
+}
+
 fetch_branch_name() {
-  branch_name="$(cat "$BRANCH_FILE")"
-  if ! git rev-parse "origin/$branch_name" > /dev/null 2>&1; then
-    echo "origin/$branch_name does not exist"
+  branch_name=$(read_tracking_branch)
+  if ! git rev-parse "$branch_name" > /dev/null 2>&1; then
+    echo "$branch_name does not exist"
     exit 1
   fi
 }
@@ -36,7 +38,7 @@ fetch_default_branch() {
 command="$1"
 case $command in
   s|stat|status)
-    cat "$BRANCH_FILE"
+    read_tracking_branch
     ;;
 
   c|create)
@@ -46,8 +48,8 @@ case $command in
     if [ -z "$last_tag" ]; then
       new_num="1"
     else
-      if [ "$(git rev-parse "$last_tag")" = "$(git rev-parse "origin/$branch_name")" ]; then
-        echo "$last_tag is same as origin"
+      if [ "$(git rev-parse "$last_tag")" = "$(git rev-parse "$branch_name")" ]; then
+        echo "$last_tag is same as upstream"
         exit 1
       fi
       last_num="${last_tag##*/}"
@@ -55,7 +57,7 @@ case $command in
     fi
 
     new_branch="review/$branch_name/$new_num"
-    git tag "$new_branch" "origin/$branch_name"
+    git tag "$new_branch" "$branch_name"
     echo "+ $new_branch"
     ;;
 
@@ -66,7 +68,7 @@ case $command in
 
   rd|range-diff)
     fetch_branch_name
-    git range-diff "origin/$(fetch_default_branch)" "review/$branch_name/$2" "review/$branch_name/$3"
+    git range-diff "$(fetch_default_branch)" "review/$branch_name/$2" "review/$branch_name/$3"
     ;;
 
   d|diff)
